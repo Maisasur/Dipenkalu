3121b9598888cd4ac60c95d62f79bc019146f6e9
Maisasur <68381095+Maisasur@users.noreply.github.com>
2021-05-31 21:16:11 +0545

    Add files via upload

---
+++ PYTHON_DAY_01.py
@@ -0,0 +1,12 @@
+print(" hello everyone this is my first code in python ")
+print(
+    '''this is an aswm language
+    i am learning it yahooo
+    i will hack nasa with python
+    haha americans here is new hacker in town hahaha
+    markzukerberg ill hack your facebook id and ill kidnap you
+
+    
+    
+    '''
+)
---
+++ python_day_2.py
@@ -0,0 +1,5 @@
+print("today i learned  about variables and how to put comments")
+price=10 #here i set the variable price with integer value 10
+print(price) #i am  printing the variable price Note:variables do not needs to be in "" or''
+ #this will bring output as 10 because price is 10
+                
